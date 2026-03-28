5859e6a35cdc527650bdc4e3368f7a990a4728a1
elkhan <elkhan.hasanli@gmail.com>
2026-03-29 01:59:46 +0400

    fix checkstyle errors(2)

--- src/main/java/com/app/config/JwtAuthFilter.java
+++ src/main/java/com/app/config/JwtAuthFilter.java
@@ -15,7 +15,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
