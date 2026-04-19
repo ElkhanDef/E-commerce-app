6526758d6c974c9eb3a94d7a87af277c2e51e0f1
elkhan <elkhan.hasanli@gmail.com>
2026-04-19 16:39:26 +0400

    refactor(product,category): improve and fix API endpoints structur

--- src/main/java/com/app/controller/ProductController.java
+++ src/main/java/com/app/controller/ProductController.java
@@ -1,15 +1,27 @@
 package com.app.controller;
 
 import com.app.model.dto.request.PageableRequest;
+import com.app.model.dto.request.ProductRequestDto;
+import com.app.model.dto.response.PartialImageUploadResponseDto;
 import com.app.model.dto.response.ProductListResponseDto;
+import com.app.model.dto.response.ProductResponseDto;
 import com.app.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/products")
@@ -24,5 +36,21 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<Page<ProductListResponseDto>> getAllProducts(@ModelAttribute @Valid PageableRequest req) {
         return ResponseEntity.ok(productService.getAllProducts(req));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody @Valid ProductRequestDto productRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productRequestDto));
+    }
+
+    @PostMapping(path = "/{productId}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PartialImageUploadResponseDto> uploadImages(@PathVariable Long productId,
+                                                                      @RequestParam("files") List<MultipartFile> files) {
+        return ResponseEntity.ok(productService.uploadImages(productId, files));
+    }
+
+    @GetMapping(path = "/id/{productId}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long productId) {
+        return ResponseEntity.ok(productService.getProductById(productId));
     }
 }
