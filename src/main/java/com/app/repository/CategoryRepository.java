b56e0c4520c61a0b0a64361b775c13765eaa9b58
elkhan <elkhan.hasanli@gmail.com>
2026-04-23 03:04:20 +0400

    feat(category): Add get by slug feature

--- src/main/java/com/app/repository/CategoryRepository.java
+++ src/main/java/com/app/repository/CategoryRepository.java
@@ -4,8 +4,12 @@ import com.app.model.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
 
     boolean existsBySlug(String slug);
+
+    Optional<CategoryEntity> findBySlug(String slug);
 }
